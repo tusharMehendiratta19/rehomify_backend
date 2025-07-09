55adcb35465086304237c35c293e7cd4c86d3b50
TusharM23 <tushar@whatsloan.in>
2025-07-09 13:10:59 +0530

    try

--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const wishlistController = require('../controllers/wishlistController');
+
 const authenticate = require('../middlewares/authenticate');
 
 // GET /api/wishlist
