772427276b1033344b8c10cda5d1ccefe7bc6a1a
TusharM23 <tushar@whatsloan.in>
2025-07-14 00:39:52 +0530

    new api changes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,6 @@ const Product = require('../models/Product');
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find({}, {
-      _id: 1,
       id: 1,
       category: 1,
       name: 1,
@@ -11,12 +10,32 @@ exports.getAllProducts = async (req, res) => {
       price: 1,
       color: 1,
       image: 1,
-      details: 1
+      deliveryTime: 1
     });
 
-    return res.status(200).json(products);
+    const categorizedProducts = {};
+
+    products.forEach(product => {
+      const key = product.category?.toLowerCase().replace(/\s/g, "_") || "uncategorized";
+
+      if (!categorizedProducts[key]) {
+        categorizedProducts[key] = [];
+      }
+
+      categorizedProducts[key].push({
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        deliveryTime: product.deliveryTime,
+        category: product.category,
+        price: product.price,
+        color: product.color,
+        image: product.image
+      });
+    });
+
+    return res.status(200).json(categorizedProducts);
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
 };
-
