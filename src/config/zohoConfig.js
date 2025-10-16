a6cd1f89d12b53a5b0f390b096df3be05b5a32f0
TusharM23 <tushar@whatsloan.in>
2025-10-17 00:26:12 +0530

    bug fix

--- src/config/zohoConfig.js
+++ src/config/zohoConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   BASE_URL: "https://payments.zoho.in/api/v1",
   ACCOUNT_ID: "60045613995",
-  auth_token: "1005.8f275a359f6cc28154bee603d10f037f.fcb5cb32f2d604afbc57c300174e8622",
+  auth_token: "1005.89712013a7d9137a1cf12926fd252bb1.e6fc25be752efe384dd0218e7947258e",
   client_id: "1005.U1V9TJQF4EST7593AYNBEOCU12OMHX",
   client_secretId: "b459474fac77f00f3dc5fd47151319e45d69894180",
-  refresh_token: "",
+  refresh_token: "1005.89712013a7d9137a1cf12926fd252bb1.e6fc25be752efe384dd0218e7947258e",
   api_key: "1003.0050a413af83c75db3f48487628e4af7.4e436d47cc165a0d9c36f9b1108ee4a5",
   api_secret_key: "7d3fdc1e594d06afc0876c5e18564bf2f59ca41fd89f694c51ff05b4c6e4dbaf78f9458fcd8e78602d2f29ecd81f97d6"
 };
